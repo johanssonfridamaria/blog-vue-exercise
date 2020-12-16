f5661db56efd8b62ca6ed7e4967c4d89ab0efc3f
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-12-16 13:48:44 +0100

    added

---
+++ src/views/Posts.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <div v-for="post in filteredPosts" :key="post.id">
+      <postsPreview :post="post" />
+    </div>
+  </div>
+</template>
+
+<script>
+import PostsPreview from '../components/PostsPreview'
+import {mapActions, mapGetters} from 'vuex';
+
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+    PostsPreview,
+  }, 
+  methods: {
+    ...mapActions(['getPosts'])
+  },
+  computed: {
+    ...mapGetters(['posts', 'filteredPosts'])
+  },
+  created(){
+    this.getPosts()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
