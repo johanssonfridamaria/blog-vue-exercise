47a3746826debfc668f1cf15341f7d93f268187c
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-12-16 13:48:34 +0100

    added form

--- src/views/AddPost.vue
+++ src/views/AddPost.vue
@@ -1,13 +1,51 @@
 <template>
-  <div>Add post</div>
+  <div class="container">
+    <form @submit.prevent="onSubmit">
+      <div class="mb-3">
+        <label for="title" class="form-label"
+          >Title</label
+        >
+        <input
+          type="email"
+          class="form-control"
+          id="exampleFormControlInput1"
+          placeholder="name@example.com"
+        />
+      </div>
+      <div class="mb-3">
+        <label for="content" class="form-label"
+          >Content</label
+        >
+        <textarea
+          class="form-control"
+          id="content"
+          rows="3"
+        ></textarea>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
+<style scoped>
+label {
+  margin-bottom: 1rem;
+}
+input {
 
+}
+textarea {
+  width: 100%;
+  height: 10rem;
+  padding: 0.7rem 1rem;
+  box-sizing: border-box;
+  border: 0.15rem solid #ccc;
+  border-radius: 0.25rem;
+  /* background-color: #f8f8f8; */
+  resize: none;
+  outline: none;
+}
 </style>
